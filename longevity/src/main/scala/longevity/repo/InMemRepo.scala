26cefc22ef7b933904dbb15ed2ee12aa1b4597d8
John Sullivan <john.sullivan.mscs@gmail.com>
2014-12-22 14:35:08 -0500

    refactor and migrate stuff from InMemRepo to Repo

--- longevity/src/main/scala/longevity/repo/InMemRepo.scala
+++ longevity/src/main/scala/longevity/repo/InMemRepo.scala
@@ -20,18 +20,11 @@ abstract class InMemRepo[E <: Entity](
   private var nextId = 0
   private var idToEntityMap = Map[Id[E], Persisted[E]]()
 
-  // TODO: can we explain why this is falling out of date with regards to updates?
-  private var originalCreations = Map[Unpersisted[E], Persisted[E]]()
-
-  def create(unpersisted: Unpersisted[E]) = {
-    val persisted = originalCreations.getOrElse(unpersisted, {
-      val id = IntId(nextId)
-      nextId += 1
-      persist(id, patchUnpersistedAssocs(unpersisted.get))
-    })
-    originalCreations += (unpersisted -> persisted)
-    persisted
-  }
+  def create(unpersisted: Unpersisted[E]) = getSessionCreationOrElse(unpersisted, {
+    val id = IntId(nextId)
+    nextId += 1
+    persist(id, patchUnpersistedAssocs(unpersisted.get))
+  })
 
   def retrieve(id: Id[E]) = idToEntityMap.getOrElse(id, NotFound(id))
 
@@ -41,29 +34,6 @@ abstract class InMemRepo[E <: Entity](
   def delete(persisted: Persisted[E]) = {
     idToEntityMap -= persisted.id
     Deleted(persisted)
-  }
-
-  private def patchUnpersistedAssocs(e: E): E = {
-    entityType.assocLenses.foldLeft(e) { (e, lens) =>
-      patchUnpersistedAssoc(e, lens)
-    }
-  }
-
-  private def patchUnpersistedAssoc[F <: Entity](e: E, lens: EntityType.AssocLens[E, F]): E = {
-    implicit val associateeTypeTag: TypeTag[F] = lens.associateeTypeTag
-    lens.patchAssoc(e, persistAssocWhenUnpersisted)
-  }
-
-  private def persistAssocWhenUnpersisted[Associatee <: Entity](assoc: Assoc[Associatee])(
-    implicit associateeTypeTag: TypeTag[Associatee]
-  ): Assoc[Associatee] = {
-    assoc match {
-      case UnpersistedAssoc(u) =>
-        val repo = repoPool.repoForEntityTypeTag(associateeTypeTag)
-        val persisted = repo.create(u)
-        persisted.id
-      case _ => assoc
-    }
   }
 
   private def persist(id: Id[E], e: E): Persisted[E]= persist(id, e, 0L)
--- longevity/src/main/scala/longevity/repo/Repo.scala
+++ longevity/src/main/scala/longevity/repo/Repo.scala
@@ -10,13 +10,7 @@ trait Repo[E <: Entity] {
   val entityTypeTag: TypeTag[E]
 
   /** the entity type for the entities this repository handles */
-  // this is not currently used, but may be soon!
   val entityType: EntityType[E]
-
-  /** the pool of all the repos in context */
-  protected val repoPool: RepoPool
-
-  repoPool.addRepo(this)
 
   /** creates the entity */
   def create(e: Unpersisted[E]): CreateResult[E]
@@ -33,4 +27,54 @@ trait Repo[E <: Entity] {
   /** deletes the entity */
   def delete(p: Persisted[E]): DeleteResult[E]
 
+  /** the pool of all the repos in context */
+  protected val repoPool: RepoPool
+
+  repoPool.addRepo(this)
+
+  /** a cache of create results for those unpersisted entities of type E that have already been created.
+   * because entities are just value objects, we expect some duplication in the unpersisted data that gets
+   * passed into [[Repo.create(Unpersisted[E])]], via the associations of created obects. we keep a session
+   * level cache of these guys to prevent multiple creation attempts on the same entity.
+   *
+   * note that this cache does not stay current with any updates or deletes to these entities! this cache
+   * is not intended for use with interleaving create/update/delete, but rather for a series of create calls. */
+  protected var sessionCreations = Map[Unpersisted[E], CreateResult[E]]()
+
+  /** pull a create result out of the cache for the given unpersisted. if it's not there, then create it,
+   * cache it, and return it */
+  protected def getSessionCreationOrElse(unpersisted: Unpersisted[E], create: => CreateResult[E])
+  : CreateResult[E] = {
+    val createResult = sessionCreations.getOrElse(unpersisted, {
+      val createResult = create
+      sessionCreations += (unpersisted -> createResult)
+      createResult
+    })
+    createResult
+  }
+
+  /** returns a version of the entity where all unpersisted associations are persisted */
+  protected def patchUnpersistedAssocs(e: E): E = {
+    entityType.assocLenses.foldLeft(e) { (e, lens) =>
+      patchUnpersistedAssoc(e, lens)
+    }
+  }
+
+  private def patchUnpersistedAssoc[F <: Entity](e: E, lens: EntityType.AssocLens[E, F]): E = {
+    implicit val associateeTypeTag: TypeTag[F] = lens.associateeTypeTag
+    lens.patchAssoc(e, persistAssocWhenUnpersisted)
+  }
+
+  private def persistAssocWhenUnpersisted[Associatee <: Entity](assoc: Assoc[Associatee])(
+    implicit associateeTypeTag: TypeTag[Associatee]
+  ): Assoc[Associatee] = {
+    assoc match {
+      case UnpersistedAssoc(u) =>
+        val repo = repoPool.repoForEntityTypeTag(associateeTypeTag)
+        val persisted = repo.create(u)
+        persisted.id
+      case _ => assoc
+    }
+  }
+  
 }
