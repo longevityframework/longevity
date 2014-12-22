f1342717ad9c5f0de15ab0e3c3463af424f0ea51
John Sullivan <john.sullivan.mscs@gmail.com>
2014-12-22 14:34:44 -0500

    add reactive mongo dependency to longevity

--- project/MusetteBuild.scala
+++ project/MusetteBuild.scala
@@ -8,6 +8,7 @@ trait BuildSettings {
     version := "0.0.0-SNAPSHOT",
     scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked"),
     scalaVersion := "2.11.4",
+    resolvers += "Typesafe repository releases" at "http://repo.typesafe.com/typesafe/releases/",
     libraryDependencies ++= Seq(
       "org.scala-lang" % "scala-reflect" % scalaVersion.value withSources() withJavadoc(),
       "org.scalatest" %% "scalatest" % "2.2.1" % "test",
@@ -28,6 +29,7 @@ object MusetteBuild extends Build with BuildSettings {
     base = file("longevity"),
     settings = buildSettings :+ (
       libraryDependencies ++= Seq(
+        "org.reactivemongo" %% "reactivemongo" % "0.10.5.0.akka23",
         "org.scalatest" %% "scalatest" % "2.2.1" % "provided")))
 
   lazy val musette = Project(
