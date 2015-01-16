5ab9220bf0754ce119b1f0eb7abd0c8bcfd130be
John Sullivan <john.sullivan.mscs@gmail.com>
2015-01-16 10:35:50 -0500

    more emblem refactoring, scaladocs

--- emblem/src/main/scala/emblem/EmblemGenerator.scala
+++ emblem/src/main/scala/emblem/EmblemGenerator.scala
@@ -3,24 +3,33 @@ package emblem
 import scala.reflect.runtime.universe._
 import emblem.stringUtil._
 
-// TODO single pass refactor
+/** a little container for all things related to emblem generation */
+object emblemGenerator {
 
-private[emblem] object emblemGenerator {
+  /** an exception indicating you broke the contract of [[emblem.emblemFor]] */
+  class EmblemGeneratorException(message: String) extends Exception(message)
 
-  @throws[TypeIsNotCaseClassException[_]]
-  @throws[CaseClassHasMultipleParamListsException[_]]
-  def emblemFor[A <: HasEmblem : TypeKey]: Emblem[A] = {
+  class TypeIsNotCaseClassException(
+    val key: TypeKey[_ <: HasEmblem])
+  extends EmblemGeneratorException(
+    s"emblems for non-case classes is currently not supported: $key")
+
+  class CaseClassHasMultipleParamListsException(
+    val key: TypeKey[_ <: HasEmblem])
+  extends EmblemGeneratorException(
+    s"emblems for case classes with extra param lists currently not supported: $key")
+
+  // TODO single pass refactor
+
+  @throws[EmblemGeneratorException]
+  private[emblem] def emblemFor[A <: HasEmblem : TypeKey]: Emblem[A] = {
     val key = implicitly[TypeKey[A]]
     val tpe = key.tpe
+    verifyIsCaseClass(tpe, key)
 
-    verifyIsCaseClass(tpe)
-    val symbol = classSymbol(tpe)
+    val symbol = tpe.typeSymbol.asClass
     val constructorSymbol = symbol.primaryConstructor.asMethod
-
-    // TODO: handle extra param lists situation
-    if (constructorSymbol.paramLists.size != 1) {
-      throw new CaseClassHasMultipleParamListsException[A]
-    }
+    verifySingleParamList(constructorSymbol, key)
       
     // case classes guaranteed to have at least one param list
     val params: List[TermSymbol] = constructorSymbol.paramLists.head.map(_.asTerm)
@@ -34,17 +43,20 @@ private[emblem] object emblemGenerator {
     new Emblem[A](typeNamePrefix(tpe), typeName(tpe), props, propDefaults, creator)
   }
 
-  private def verifyIsCaseClass[A <: HasEmblem : TypeKey](tpe: Type): Unit = {
+  @throws[TypeIsNotCaseClassException]
+  private def verifyIsCaseClass(tpe: Type, key: TypeKey[_ <: HasEmblem]): Unit = {
     val symbol = tpe.typeSymbol
-    if (!symbol.isClass) {
-      throw new TypeIsNotCaseClassException[A]
-    }
-    if (!symbol.asClass.isCaseClass) {
-      throw new TypeIsNotCaseClassException[A]
+    if (!symbol.isClass || !symbol.asClass.isCaseClass) {
+      throw new TypeIsNotCaseClassException(key)
     }
   }
 
-  private def classSymbol(tpe: Type): ClassSymbol = tpe.typeSymbol.asClass
+  @throws[CaseClassHasMultipleParamListsException]
+  private def verifySingleParamList(constructorSymbol: MethodSymbol, key: TypeKey[_ <: HasEmblem]): Unit = {
+    if (constructorSymbol.paramLists.size != 1) {
+      throw new CaseClassHasMultipleParamListsException(key)
+    }
+  }
 
   private def emblemProp[A <: HasEmblem : TypeKey](tpe: Type, name: TermName): EmblemProp[A, _] = {
     val memberTerm = tpe.member(name).asTerm.accessed.asTerm
--- emblem/src/test/scala/emblem/EmblemForSpec.scala
+++ emblem/src/test/scala/emblem/EmblemForSpec.scala
@@ -3,7 +3,7 @@ package emblem
 import org.scalatest._
 import org.scalatest.OptionValues._
 
-/** [[emblemFor emblemFor]] specifications */
+/** [[emblemFor emblem.emblemFor]] specifications */
 class EmblemForSpec extends FlatSpec with GivenWhenThen with Matchers {
 
   behavior of "emblem.emblemFor"
@@ -11,7 +11,7 @@ class EmblemForSpec extends FlatSpec with GivenWhenThen with Matchers {
   trait Foo extends HasEmblem
 
   it should "throw exception on non case class types" in {
-    intercept[TypeIsNotCaseClassException[_]] {
+    intercept[emblemGenerator.TypeIsNotCaseClassException] {
       emblemFor[Foo]
     }
   }
@@ -19,7 +19,7 @@ class EmblemForSpec extends FlatSpec with GivenWhenThen with Matchers {
   case class Bar(i: Int)(j: Int) extends HasEmblem
 
   it should "throw exception on case classes with multiple param lists" in {
-    intercept[CaseClassHasMultipleParamListsException[_]] {
+    intercept[emblemGenerator.CaseClassHasMultipleParamListsException] {
       emblemFor[Bar]
     }
   }
