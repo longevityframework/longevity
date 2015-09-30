5e8d4e0b7ecffec453b815a5297a87f04bae67b5
John Sullivan <john.sullivan.mscs@gmail.com>
2015-09-29 21:18:48 -0500

    remove a todo comment
    it's moved to the board: https://www.pivotaltracker.com/story/show/104496450

--- longevity/src/test/scala/longevity/integration/master/MongoMasterSpec.scala
+++ longevity/src/test/scala/longevity/integration/master/MongoMasterSpec.scala
@@ -5,8 +5,6 @@ import longevity.MasterIntegrationTest
 
 import org.scalatest.Suites
 
-// TODO: build commands such as master:test are broken (no scenarios run) please fix
-
 @IntegrationTest
 @MasterIntegrationTest
 class MongoMasterSpec extends Suites(context.longevityContext.repoPoolSpec)
