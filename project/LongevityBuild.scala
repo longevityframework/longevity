336f8f5519556fa5bd3230d13c0a4c8c663b2b09
John Sullivan <john.sullivan.mscs@gmail.com>
2015-10-19 19:41:02 -0500

    change scalatest dep from provided to optional

--- project/LongevityBuild.scala
+++ project/LongevityBuild.scala
@@ -114,7 +114,7 @@ object LongevityBuild extends Build with BuildSettings {
     settings = buildSettings ++ Seq(
       version := "0.2-SNAPSHOT",
       libraryDependencies += "org.mongodb" %% "casbah" % "2.8.2",
-      libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.1" % "provided",
+      libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.1" % Optional,
       homepage := Some(url("https://github.com/sullivan-/emblem")),
       pomExtra := (
         <scm>
