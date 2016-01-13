b3f0b37cfbe85f26ac503ca2037daa79b7a61462
John Sullivan <john.sullivan.mscs@gmail.com>
2016-01-12 23:32:33 -0600

    up version to 0.4.1

--- project/LongevityBuild.scala
+++ project/LongevityBuild.scala
@@ -29,7 +29,7 @@ trait BuildSettings {
 
   val buildSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "org.longevityframework",
-    version := "0.4.0",
+    version := "0.4.1",
     scalaVersion := "2.11.7",
 
     // compile
