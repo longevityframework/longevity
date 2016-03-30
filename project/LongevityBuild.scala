5535b8743fc65f597c15148e31c26038be7a7846
John Sullivan <john.sullivan.mscs@gmail.com>
2016-03-30 13:12:49 -0500

    up version to 0.7-SNAPSHOT

--- project/LongevityBuild.scala
+++ project/LongevityBuild.scala
@@ -24,7 +24,7 @@ trait BuildSettings {
 
   val publishSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "org.longevityframework",
-    version := "0.6.0",
+    version := "0.7-SNAPSHOT",
     scalaVersion := "2.11.7",
 
     publishMavenStyle := true,
