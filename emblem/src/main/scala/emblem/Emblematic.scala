0c67c8cc8ec5f3d0c1fdd3aa4a2d463ac1486c74
John Sullivan <john.sullivan.mscs@gmail.com>
2016-04-14 18:17:39 -0500

    extend Emblematic to include Unions

--- emblem/src/main/scala/emblem/Emblematic.scala
+++ emblem/src/main/scala/emblem/Emblematic.scala
@@ -1,14 +1,19 @@
 package emblem
 
-/** describes a collection of types used in composite data structures.
- * along with all of the [[basicTypes basic types]] and supported collections
- * (`Option`, `Set`, and `Map`), a collection of [[Emblem emblems]] and
- * [[Extractor extractors]] describe case classes found in the
- * data structures.
+/** describes a collection of types used in composite data structures. these
+ * data structures can contain all of the following types:
+ * 
+ * - all of the [[basicTypes basic types]]
+ * - supported collections. currently `Option`, `Set`, and `Map`
+ * - case classes represented in the collection of [[Emblem emblems]]
+ * - case classes represented in the collection of [[Extractor extractors]]
+ * - traits represented in the collection of [[Union unions]]
  *
- * @param emblems the emblems to use in the traversal. defaults to empty
- * @param extractors the extractors to use in the traversal. defaults to empty
+ * @param emblems the emblems to use in the emblematic. defaults to empty
+ * @param extractors the extractors to use in the emblematic. defaults to empty
+ * @param unions the unions to use in the emblematic. defaults to empty
  */
 case class Emblematic(
   emblems: EmblemPool = EmblemPool.empty,
-  extractors: ExtractorPool = ExtractorPool.empty)
+  extractors: ExtractorPool = ExtractorPool.empty,
+  unions: UnionPool = UnionPool.empty)
