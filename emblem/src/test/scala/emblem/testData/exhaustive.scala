4f0084c96c6c14bd89e342582d9b1d4b040bd077
John Sullivan <john.sullivan.mscs@gmail.com>
2016-04-15 11:07:09 -0500

    flesh out exhaustive & JsonTranslationSpec

--- emblem/src/test/scala/emblem/testData/exhaustive.scala
+++ emblem/src/test/scala/emblem/testData/exhaustive.scala
@@ -1,12 +1,13 @@
 package emblem.testData
 
-//import emblem.Emblem
-//import emblem.HasEmblem
 import com.github.nscala_time.time.Imports.richDateTime
+import emblem.Emblem
 import emblem.EmblemPool
 import emblem.Emblematic
 import emblem.Extractor
 import emblem.ExtractorPool
+import emblem.HasEmblem
+import emblem.traversors.sync.TestDataGenerator
 import org.joda.time.DateTime
 
 /** an attempt at an exhaustive set of data to cover emblematic traversal logic */
@@ -21,24 +22,129 @@ object exhaustive {
   val uriExtractor = Extractor[Uri, String]
 
   lazy val extractorPool = ExtractorPool(emailExtractor, markdownExtractor, uriExtractor)
-  lazy val emblemPool = EmblemPool()
+
+  case class WithBasics(
+    boolean: Boolean,
+    char: Char,
+    dateTime: DateTime,
+    double: Double,
+    float: Float,
+    int: Int,
+    long: Long,
+    string: String)
+  extends HasEmblem
+
+  case class WithExtractors(
+    email: Email,
+    markdown: Markdown,
+    uri: Uri)
+  extends HasEmblem
+
+  case class WithCollections(
+    option: Option[String],
+    set: Set[String],
+    list: List[String])
+  extends HasEmblem
+
+  lazy val emblemPool = EmblemPool(
+    Emblem[WithBasics],
+    Emblem[WithExtractors],
+    Emblem[WithCollections])
+
   lazy val emblematic = Emblematic(extractorPool, emblemPool)
 
+  lazy val generator = new TestDataGenerator(emblematic)
+
   object basics {
-    val boolean: Boolean = true
-    val char: Char = '*'
-    val dateTime: DateTime = DateTime.now
-    val double: Double = 0.234d
-    val float: Float = 0.2341234f
-    val int: Int = 876324
-    val long: Long = -187346
-    val string: String = "asdjhfbsvy9248ywjfsdczada;;;;"
+    def boolean = generator.generate[Boolean]
+    def char = generator.generate[Char]
+    def dateTime = generator.generate[DateTime]
+    def double = generator.generate[Double]
+    def float = generator.generate[Float]
+    def int = generator.generate[Int]
+    def long = generator.generate[Long]
+    def string = generator.generate[String]
   }
 
   object extractors {
-    val email = Email("f@g.z")
-    val markdown = Markdown("sfgsfdg")
-    val uri = Uri("httpcolonslashslash")
+    def email = generator.generate[Email]
+    def markdown = generator.generate[Markdown]
+    def uri = generator.generate[Uri]
+  }
+
+  object emblems {
+    def withBasics = generator.generate[WithBasics]
+    def withExtractors = generator.generate[WithExtractors]
+    def withCollections = generator.generate[WithCollections]
+  }
+
+  object options {
+    def boolean = generator.generate[Option[Boolean]]
+    def char = generator.generate[Option[Char]]
+    def dateTime = generator.generate[Option[DateTime]]
+    def double = generator.generate[Option[Double]]
+    def float = generator.generate[Option[Float]]
+    def int = generator.generate[Option[Int]]
+    def long = generator.generate[Option[Long]]
+    def string = generator.generate[Option[String]]
+
+    def email = generator.generate[Option[Email]]
+    def markdown = generator.generate[Option[Markdown]]
+    def uri = generator.generate[Option[Uri]]
+
+    def withBasics = generator.generate[Option[WithBasics]]
+    def withExtractors = generator.generate[Option[WithExtractors]]
+    def withCollections = generator.generate[Option[WithCollections]]
+
+    def option = generator.generate[Option[Option[String]]]
+    def set = generator.generate[Option[Set[String]]]
+    def list = generator.generate[Option[List[String]]]
+  }
+
+  object sets {
+    def boolean = generator.generate[Set[Boolean]]
+    def char = generator.generate[Set[Char]]
+    def dateTime = generator.generate[Set[DateTime]]
+    def double = generator.generate[Set[Double]]
+    def float = generator.generate[Set[Float]]
+    def int = generator.generate[Set[Int]]
+    def long = generator.generate[Set[Long]]
+    def string = generator.generate[Set[String]]
+
+    def email = generator.generate[Set[Email]]
+    def markdown = generator.generate[Set[Markdown]]
+    def uri = generator.generate[Set[Uri]]
+
+    def withBasics = generator.generate[Set[WithBasics]]
+    def withExtractors = generator.generate[Set[WithExtractors]]
+    def withCollections = generator.generate[Set[WithCollections]]
+
+    def option = generator.generate[Set[Option[String]]]
+    def set = generator.generate[Set[Set[String]]]
+    def list = generator.generate[Set[List[String]]]
+  }
+
+  object lists {
+    def boolean = generator.generate[List[Boolean]]
+    def char = generator.generate[List[Char]]
+    def dateTime = generator.generate[List[DateTime]]
+    def double = generator.generate[List[Double]]
+    def float = generator.generate[List[Float]]
+    def int = generator.generate[List[Int]]
+    def long = generator.generate[List[Long]]
+    def string = generator.generate[List[String]]
+
+    def email = generator.generate[List[Email]]
+    def markdown = generator.generate[List[Markdown]]
+    def uri = generator.generate[List[Uri]]
+
+    def withBasics = generator.generate[List[WithBasics]]
+    def withExtractors = generator.generate[List[WithExtractors]]
+    def withCollections = generator.generate[List[WithCollections]]
+
+    def option = generator.generate[List[Option[String]]]
+    def set = generator.generate[List[Set[String]]]
+    def list = generator.generate[List[List[String]]]
   }
 
 }
--- emblem/src/test/scala/emblem/traversors/sync/JsonTranslationSpec.scala
+++ emblem/src/test/scala/emblem/traversors/sync/JsonTranslationSpec.scala
@@ -8,7 +8,7 @@ import org.scalatest.Matchers
 
 /** exercises translations of [[JsonToEmblemTranslator]] and
  * [[EmblemToJsonTranslator]] by running data through both, and seeing if we
- * get the original result.
+ * get the original result. also exercises the [[TestDataGenerator]].
  *
  * this test attempts to be exhaustive of emblematic features using
  * [[emblem.testData.exhaustive]].
@@ -42,9 +42,82 @@ class JsonTranslationSpec extends FlatSpec with GivenWhenThen with Matchers {
     pipelineReproducesInput(exhaustive.extractors.uri)
   }
 
-  // TODO: emblems
+  it should "produce the original output for emblems" in {
+    pipelineReproducesInput(exhaustive.emblems.withBasics)
+    pipelineReproducesInput(exhaustive.emblems.withExtractors)
+    pipelineReproducesInput(exhaustive.emblems.withCollections)
+  }
+
   // TODO: unions
-  // TODO: collections
+
+  it should "produce the original output for options" in {
+    pipelineReproducesInput(exhaustive.options.boolean)
+    pipelineReproducesInput(exhaustive.options.char)
+    pipelineReproducesInput(exhaustive.options.dateTime)
+    pipelineReproducesInput(exhaustive.options.double)
+    pipelineReproducesInput(exhaustive.options.float)
+    pipelineReproducesInput(exhaustive.options.int)
+    pipelineReproducesInput(exhaustive.options.long)
+    pipelineReproducesInput(exhaustive.options.string)
+
+    pipelineReproducesInput(exhaustive.options.email)
+    pipelineReproducesInput(exhaustive.options.markdown)
+    pipelineReproducesInput(exhaustive.options.uri)
+
+    pipelineReproducesInput(exhaustive.options.withBasics)
+    pipelineReproducesInput(exhaustive.options.withExtractors)
+    pipelineReproducesInput(exhaustive.options.withCollections)
+
+    pipelineReproducesInput(exhaustive.options.option.flatten) // JSON rep does not respect Some(None)
+    pipelineReproducesInput(exhaustive.options.set)
+    pipelineReproducesInput(exhaustive.options.list)
+  }
+
+  it should "produce the original output for sets" in {
+    pipelineReproducesInput(exhaustive.sets.boolean)
+    pipelineReproducesInput(exhaustive.sets.char)
+    pipelineReproducesInput(exhaustive.sets.dateTime)
+    pipelineReproducesInput(exhaustive.sets.double)
+    pipelineReproducesInput(exhaustive.sets.float)
+    pipelineReproducesInput(exhaustive.sets.int)
+    pipelineReproducesInput(exhaustive.sets.long)
+    pipelineReproducesInput(exhaustive.sets.string)
+
+    pipelineReproducesInput(exhaustive.sets.email)
+    pipelineReproducesInput(exhaustive.sets.markdown)
+    pipelineReproducesInput(exhaustive.sets.uri)
+
+    pipelineReproducesInput(exhaustive.sets.withBasics)
+    pipelineReproducesInput(exhaustive.sets.withExtractors)
+    pipelineReproducesInput(exhaustive.sets.withCollections)
+
+    pipelineReproducesInput(exhaustive.sets.option)
+    pipelineReproducesInput(exhaustive.sets.set)
+    pipelineReproducesInput(exhaustive.sets.list)
+  }
+
+  it should "produce the original output for lists" in {
+    pipelineReproducesInput(exhaustive.lists.boolean)
+    pipelineReproducesInput(exhaustive.lists.char)
+    pipelineReproducesInput(exhaustive.lists.dateTime)
+    pipelineReproducesInput(exhaustive.lists.double)
+    pipelineReproducesInput(exhaustive.lists.float)
+    pipelineReproducesInput(exhaustive.lists.int)
+    pipelineReproducesInput(exhaustive.lists.long)
+    pipelineReproducesInput(exhaustive.lists.string)
+
+    pipelineReproducesInput(exhaustive.lists.email)
+    pipelineReproducesInput(exhaustive.lists.markdown)
+    pipelineReproducesInput(exhaustive.lists.uri)
+
+    pipelineReproducesInput(exhaustive.lists.withBasics)
+    pipelineReproducesInput(exhaustive.lists.withExtractors)
+    pipelineReproducesInput(exhaustive.lists.withCollections)
+
+    pipelineReproducesInput(exhaustive.lists.option)
+    pipelineReproducesInput(exhaustive.lists.set)
+    pipelineReproducesInput(exhaustive.lists.list)
+  }
 
   private def pipelineReproducesInput[A : TypeKey](a: A): Unit = {
     jsonTranslator.traverse(emblemTranslator.traverse[A](a)) should equal (a)
