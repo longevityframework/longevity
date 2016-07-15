17d8921089bb8bcfcfbce0c83454f0c50bac976a
John Sullivan <john.sullivan.mscs@gmail.com>
2016-07-15 14:27:21 -0500

    up build version to 0.9.0

--- project/LongevityBuild.scala
+++ project/LongevityBuild.scala
@@ -24,7 +24,7 @@ trait BuildSettings {
 
   val publishSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "org.longevityframework",
-    version := "0.9-SNAPSHOT",
+    version := "0.9.0",
     scalaVersion := "2.11.8",
 
     publishMavenStyle := true,
