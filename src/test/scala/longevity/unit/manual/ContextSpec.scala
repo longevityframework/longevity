c03bb47f5812781af85bfda79540955faa629878
John Sullivan <john.sullivan.mscs@gmail.com>
2016-07-29 16:58:20 -0500

    add LongevityConfig user manual example

--- src/test/scala/longevity/unit/manual/ContextSpec.scala
+++ src/test/scala/longevity/unit/manual/ContextSpec.scala
@@ -10,7 +10,7 @@ import org.scalatest.Matchers
  */
 object ContextSpec {
 
-  object config {
+  object config1 {
 
     import com.typesafe.config.Config
     import longevity.context.Cassandra
@@ -41,6 +41,48 @@ object ContextSpec {
     def loadAccountsConfig(): Config = ConfigFactory.load()
   }
 
+  object config2 {
+
+    import longevity.subdomain.CoreDomain
+    import longevity.subdomain.ptype.PTypePool
+
+    val bloggingDomain: CoreDomain =
+      CoreDomain("blogging", PTypePool.empty)
+
+    import longevity.context.LongevityConfig
+    import longevity.context.MongoConfig
+    import longevity.context.TestConfig
+    import longevity.context.CassandraConfig
+
+    val longevityConfig = LongevityConfig(
+      optimisticLocking = true,
+      mongodb = MongoConfig(
+        uri = "localhost:27017",
+        db = "longevity_main"),
+      cassandra = CassandraConfig(
+        address = "localhost",
+        credentials = None,
+        keyspace = "longevity_main",
+        replicationFactor = 1),
+      test = TestConfig(
+        mongodb = MongoConfig(
+          uri = "localhost:27017",
+          db = "longevity_test"),
+        cassandra = CassandraConfig(
+          address = "localhost",
+          credentials = None,
+          keyspace = "longevity_test",
+          replicationFactor = 1)))
+
+    import longevity.context.LongevityContext
+    import longevity.context.Mongo
+
+    val bloggingContext = new LongevityContext(
+      bloggingDomain,
+      Mongo,
+      config = longevityConfig)
+  }
+
 }
 
 /** exercises code samples found in the context section of the user manual. the samples themselves are
@@ -54,8 +96,9 @@ class ContextSpec extends FlatSpec with GivenWhenThen with Matchers {
   import ContextSpec._
 
   "user manual example code" should "compile" in {
-    config.bloggingContext.subdomain.name should equal ("blogging")
-    config.accountsContext.subdomain.name should equal ("accounts")
+    config1.bloggingContext.subdomain.name should equal ("blogging")
+    config1.accountsContext.subdomain.name should equal ("accounts")
+    config2.bloggingContext.subdomain.name should equal ("blogging")
   }
 
 }
