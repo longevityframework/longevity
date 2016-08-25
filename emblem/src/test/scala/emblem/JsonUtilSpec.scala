0fb280c1ad3e10719d8e7331c9b309d30f018129
John Sullivan <john.sullivan.mscs@gmail.com>
2016-08-25 16:12:14 -0500

    fix jsonUtil.dateTimeFormatter again
    this time to respect things like "Etc/UTC"

--- emblem/src/test/scala/emblem/JsonUtilSpec.scala
+++ emblem/src/test/scala/emblem/JsonUtilSpec.scala
@@ -1,6 +1,7 @@
 package emblem
 
 import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
 import org.scalatest.FlatSpec
 import org.scalatest.GivenWhenThen
 import org.scalatest.Matchers
@@ -13,6 +14,20 @@ class JsonUtilSpec extends FlatSpec with GivenWhenThen with Matchers {
 
   it should "produce the original DateTime in a round trip" in {
     val expected = DateTime.now
+    val stringRep = dateTimeFormatter.print(expected)
+    val actual = dateTimeFormatter.parseDateTime(stringRep)
+    actual should equal (expected)
+  }
+
+  it should "produce the original DateTime in a round trip when time zone is set to UTC" in {
+    val expected = DateTime.now.withZone(DateTimeZone.UTC)
+    val stringRep = dateTimeFormatter.print(expected)
+    val actual = dateTimeFormatter.parseDateTime(stringRep)
+    actual should equal (expected)
+  }
+
+  it should "produce the original DateTime in a round trip when time zone is set to Etc/UTC" in {
+    val expected = DateTime.now.withZone(DateTimeZone.forID("Etc/UTC"))
     val stringRep = dateTimeFormatter.print(expected)
     val actual = dateTimeFormatter.parseDateTime(stringRep)
     actual should equal (expected)
