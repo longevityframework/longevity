26eb1e3d1e83523446a44166f2fd16c04442ae69
John Sullivan <john.sullivan.mscs@gmail.com>
2016-10-04 13:53:09 -0500

    add scaladoc for two new packages

---
+++ src/main/scala/longevity/ddd/package.scala
@@ -0,0 +1,14 @@
+package longevity
+
+/** provides DDD extensions to longevity.
+ *
+ * this is more or less a holding ground for the moment.
+ *
+ * there are a number of synonym terms here for some of the types in
+ * `longevity.subdomain`. none of them add any functionality to their parent
+ * classes yet.
+ */
+package object ddd {
+
+}
+
---
+++ src/main/scala/longevity/ddd/subdomain/package.scala
@@ -0,0 +1,7 @@
+package longevity.ddd
+
+/** provides DDD extensions for constructing your subdomain. */
+package object subdomain {
+
+}
+
