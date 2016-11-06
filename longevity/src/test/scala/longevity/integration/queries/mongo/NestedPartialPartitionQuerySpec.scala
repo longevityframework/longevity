17718e391ed480ff9f70a9db5091f8d2b7a46db1
John Sullivan <john.sullivan.mscs@gmail.com>
2016-11-05 20:12:21 -0500

    third query test for partial partitions

---
+++ longevity/src/test/scala/longevity/integration/queries/mongo/NestedPartialPartitionQuerySpec.scala
@@ -0,0 +1,27 @@
+package longevity.integration.queries.mongo
+
+import longevity.TestLongevityConfigs
+import longevity.context.LongevityContext
+import longevity.test.QuerySpec
+import longevity.integration.subdomain.partitionKeyInComponentWithPartialPartition._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class NestedPartialPartitionQuerySpec extends QuerySpec[PKInComponentWithPartialPartition](
+  new LongevityContext(subdomain, TestLongevityConfigs.mongoConfig)) {
+
+  lazy val sample = randomP
+  val keyProp = PKInComponentWithPartialPartition.props.key
+  import PKInComponentWithPartialPartition.queryDsl._
+
+  behavior of "MongoRepo.retrieveByQuery"
+
+  it should "produce expected results for simple equality queries" in {
+    exerciseQuery(keyProp eqs sample.component.key)
+    exerciseQuery(keyProp neq sample.component.key)
+    exerciseQuery(keyProp lt  sample.component.key)
+    exerciseQuery(keyProp lte sample.component.key)
+    exerciseQuery(keyProp gt  sample.component.key)
+    exerciseQuery(keyProp gte sample.component.key)
+  }
+
+}
