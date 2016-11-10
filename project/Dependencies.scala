5726243a7b061fb892ad8699996797b8b2db3d08
John Sullivan <john.sullivan.mscs@gmail.com>
2016-11-10 08:58:21 -0600

    up a couple dependencies to latest versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,8 @@ object Dependencies {
   val scalaVersionString = "2.11.8"
 
   val akkaStreamDep:     ModuleID = "com.typesafe.akka"          %% "akka-stream"           % "2.4.12"
-  val cassandraDep:      ModuleID = "com.datastax.cassandra"     %  "cassandra-driver-core" % "3.1.1"
-  val json4sDep:         ModuleID = "org.json4s"                 %% "json4s-native"         % "3.4.2"
+  val cassandraDep:      ModuleID = "com.datastax.cassandra"     %  "cassandra-driver-core" % "3.1.2"
+  val json4sDep:         ModuleID = "org.json4s"                 %% "json4s-native"         % "3.5.0"
   val kxbmapConfigsDep:  ModuleID = "com.github.kxbmap"          %% "configs"               % "0.4.4"
   val mongodbDep:        ModuleID = "org.mongodb"                %  "mongodb-driver"        % "3.3.0"
   val nScalaTimeDep:     ModuleID = "com.github.nscala-time"     %% "nscala-time"           % "2.14.0"
