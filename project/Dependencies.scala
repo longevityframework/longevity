4147d7cdf88734947e94cf8019e51b51050f9e6b
John Sullivan <john.sullivan.mscs@gmail.com>
2016-11-11 11:18:07 -0600

    cross build to scala 2.11.8 & 2.12.0
    also, up scalatest to 3.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,7 @@ import Keys._
 
 object Dependencies {
 
-  //val scalaVersionString = "2.12.0"
-  val scalaVersionString = "2.11.8"
+  val scalaVersionString = "2.12.0"
 
   val akkaStreamDep:     ModuleID = "com.typesafe.akka"          %% "akka-stream"           % "2.4.12"
   val cassandraDep:      ModuleID = "com.datastax.cassandra"     %  "cassandra-driver-core" % "3.1.2"
@@ -13,8 +12,7 @@ object Dependencies {
   val mongodbDep:        ModuleID = "org.mongodb"                %  "mongodb-driver"        % "3.3.0"
   val nScalaTimeDep:     ModuleID = "com.github.nscala-time"     %% "nscala-time"           % "2.14.0"
   val scalaLoggingDep:   ModuleID = "com.typesafe.scala-logging" %% "scala-logging"         % "3.5.0"
-  val scalaReflectDep:   ModuleID = "org.scala-lang"             %  "scala-reflect"         % scalaVersionString
-  val scalaTestDep:      ModuleID = "org.scalatest"              %% "scalatest"             % "2.2.6"
+  val scalaTestDep:      ModuleID = "org.scalatest"              %% "scalatest"             % "3.0.1"
   val slf4jSimpleDep:    ModuleID = "org.slf4j"                  %  "slf4j-simple"          % "1.7.21"
   val typesafeConfigDep: ModuleID = "com.typesafe"               %  "config"                % "1.3.1"
 
