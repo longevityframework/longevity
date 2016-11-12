486ca066b0ae99457e9175ff17546244f3fa4793
John Sullivan <john.sullivan.mscs@gmail.com>
2016-11-12 11:01:33 -0600

    fix compiler errors is longevity-ddd project

--- longevity-ddd/src/main/scala/longevity/ddd/subdomain/EntityType.scala
+++ longevity-ddd/src/main/scala/longevity/ddd/subdomain/EntityType.scala
@@ -3,14 +3,13 @@ package longevity.ddd.subdomain
 import emblem.TypeKey
 import longevity.subdomain.EType
 
-/** a type class for a [[Entity domain entity]] */
-abstract class EntityType[E <: Entity : TypeKey] extends EType[E]
+/** a type class for a domain entity */
+abstract class EntityType[E : TypeKey] extends EType[E]
 
 /** contains a factory method for creating `EntityTypes` */
 object EntityType {
 
   /** create and return an `EntityType` for type `E` */
-  def apply[E <: Entity : TypeKey] = new EntityType[E] {
-  }
+  def apply[E : TypeKey] = new EntityType[E] {}
 
 }
--- longevity-ddd/src/main/scala/longevity/ddd/subdomain/ValueType.scala
+++ longevity-ddd/src/main/scala/longevity/ddd/subdomain/ValueType.scala
@@ -4,13 +4,12 @@ import emblem.TypeKey
 import longevity.subdomain.EType
 
 /** a value type. functionally equivalent to an [[EntityType]] */
-abstract class ValueType[A <: ValueObject : TypeKey] extends EType[A]
+abstract class ValueType[A : TypeKey] extends EType[A]
 
 /** contains a factory method for creating `ValueTypes` */
 object ValueType {
 
   /** create and return an `ValueType` for type `A` */
-  def apply[A <: ValueObject : TypeKey] = new ValueType[A] {
-  }
+  def apply[A : TypeKey] = new ValueType[A] {}
 
 }
