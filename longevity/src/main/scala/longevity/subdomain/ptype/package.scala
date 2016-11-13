b3e0c5c314a03970bb57f97992e12d8edf0e573d
John Sullivan <john.sullivan.mscs@gmail.com>
2016-11-13 17:17:38 -0600

    hide a way a couple internal types

--- longevity/src/main/scala/longevity/subdomain/ptype/package.scala
+++ longevity/src/main/scala/longevity/subdomain/ptype/package.scala
@@ -4,9 +4,9 @@ package longevity.subdomain
 package object ptype {
 
   /** an arbitrary [[Key key]] type for a given persistent type `P` */
-  type AnyKey[P] = Key[P, V] forSome { type V <: KeyVal[P] }
+  private[longevity] type AnyKey[P] = Key[P, V] forSome { type V <: KeyVal[P] }
 
   /** an arbitrary [[PartitionKey partition key]] type for a given persistent type `P` */
-  type AnyPartitionKey[P] = PartitionKey[P, V] forSome { type V <: KeyVal[P] }
+  private[longevity] type AnyPartitionKey[P] = PartitionKey[P, V] forSome { type V <: KeyVal[P] }
 
 }
