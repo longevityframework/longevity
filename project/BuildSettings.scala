ddc830c7fc51d1877d1b6c4985ec3809fa83910a
John Sullivan <john.sullivan.mscs@gmail.com>
2016-12-07 23:51:57 -0600

    up build version to 0.19-SNAPSHOT

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,7 @@ object BuildSettings {
 
   val commonSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "org.longevityframework",
-    version := "0.18.0",
+    version := "0.19-SNAPSHOT",
     scalaVersion := Dependencies.scalaVersionString,
     crossScalaVersions := Seq("2.11.8", "2.12.0"))
 
