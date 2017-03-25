6b10b99202bd53a2e0a56426b40a00e640108aa4
John Sullivan <john.sullivan.mscs@gmail.com>
2017-03-25 10:22:05 -0500

    revert version to 0.22-SNAP..
    ..after interrupted release script

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,7 @@ object BuildSettings {
 
   val commonSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "org.longevityframework",
-    version := "0.22.0",
+    version := "0.22-SNAPSHOT",
     scalaVersion := Dependencies.scalaVersionString,
     crossScalaVersions := Seq("2.11.8", Dependencies.scalaVersionString))
 
