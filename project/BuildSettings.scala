98d0455231612e5394a6ae35ca79df8fa61e22d7
John Sullivan <john.sullivan.mscs@gmail.com>
2017-06-09 16:00:48 -0500

    up longevity version to 0.23-SNAPSHOT

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,7 @@ object BuildSettings {
 
   val commonSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "org.longevityframework",
-    version := "0.23.0",
+    version := "0.24-SNAPSHOT",
     scalaVersion := Dependencies.scalaVersionString,
     crossScalaVersions := Seq("2.11.11", Dependencies.scalaVersionString))
 
